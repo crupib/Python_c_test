064542a336fcaff17420acdf868c331e91e308a5
crupib <crupib@gmail.com>
2018-03-27 22:56:18 -0400

    Initial Commit

---
+++ Python_c_test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Python_c_test
+//
+//  Created by William Crupi on 3/27/18.
+//  Copyright © 2018 William Crupi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
