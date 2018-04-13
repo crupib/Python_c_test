da87a233d5e0dd4c799734143b8bf93ba528cbc3
crupib <crupib@gmail.com>
2018-04-12 21:54:34 -0400

    Python c test

--- Python_c_test/main.c
+++ Python_c_test/main.c
@@ -7,9 +7,15 @@
 //
 
 #include <stdio.h>
-
+#include "/Library/Frameworks/Python.framework/Versions/3.6/include/python3.6m/Python.h"
 int main(int argc, const char * argv[]) {
     // insert code here...
+    Py_Initialize();
     printf("Hello, World!\n");
+    PyRun_SimpleString("import sys\n");
+    PyRun_SimpleString("print sys.builtin_module_names\n");
+    PyRun_SimpleString("x = 'brave '+ 'sir robin'");
+    PyRun_SimpleString("print(x)");
+
     return 0;
 }
